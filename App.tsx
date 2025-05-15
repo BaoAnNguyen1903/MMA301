e2f4aec3e860d07f9e85ae6953efd8891e09d51e
An Nguyen <anndb1903@gmail.com>
2025-05-15 10:23:27 +0700

    Image

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from "react-native";
 
 const onPressButton = () => {
   Alert.alert("dasdhasidah");
@@ -8,28 +16,18 @@ const onPressButton = () => {
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ marginBottom: 12 }}>UserName:</Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12
-        }}
-        placeholder="Enter username"
+      <Image
+        source={require("./assets/icon.png")}
+        style={{ width: 200, height: 200 }}
       />
-      <Text style={{ marginBottom: 12 }}>Password:</Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          marginBottom: 12
+
+      <Image
+        source={{
+          uri: "https://icdn.dantri.com.vn/2017/khieu-dam-tre-em-1-1489518122023.jpg"
         }}
-        placeholder="Enter username"
-        secureTextEntry={true}
+        style={{ width: 200, height: 200 }}
+        resizeMode="contain"
       />
-      <Button title="Submit" onPress={onPressButton} />
     </View>
   );
 }
