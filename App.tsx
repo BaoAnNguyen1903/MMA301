36b886801f27201895d0b90bf32ba85806718ee5
An Nguyen <anndb1903@gmail.com>
2025-05-15 10:16:13 +0700

    Text Input

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 const onPressButton = () => {
   Alert.alert("dasdhasidah");
@@ -8,15 +8,28 @@ const onPressButton = () => {
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Demo Button</Text>
-      <Button title="Click Me" color="red" onPress={() => alert("me")} />
-      <Button
-        title="Click Me"
-        color="red"
-        disabled={false}
-        onPress={onPressButton}
+      <Text style={{ marginBottom: 12 }}>UserName:</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 12
+        }}
+        placeholder="Enter username"
       />
-      <Button title="Click Me" color="red" disabled={true} />
+      <Text style={{ marginBottom: 12 }}>Password:</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 12
+        }}
+        placeholder="Enter username"
+        secureTextEntry={true}
+      />
+      <Button title="Submit" onPress={onPressButton} />
     </View>
   );
 }
@@ -25,7 +38,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    // alignItems: "center",
+    justifyContent: "center",
+    padding: 16
   }
 });
