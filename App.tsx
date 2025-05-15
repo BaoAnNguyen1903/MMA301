c45e2261bda82885546c18b99cd5a3638ccf3973
An Nguyen <anndb1903@gmail.com>
2025-05-15 10:03:42 +0700

    Text and View

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>
+        Hello World
+      </Text>
     </View>
   );
 }
