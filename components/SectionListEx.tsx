bbe893a501d6945ab0e82e6f3af36804084023a0
An Nguyen <anndb1903@gmail.com>
2025-05-16 22:17:43 +0700

    update

--- components/SectionListEx.tsx
+++ components/SectionListEx.tsx
@@ -1,11 +1,35 @@
-import { View } from "react-native"
+import { SectionList, Text, View } from "react-native";
 
-const SectionListEx = () => { 
-return (
+const Data = [
+  {
+    title: "Trai Cay",
+    data: ["Tao", "Banana", "Cam"]
+  },
+  {
+    title: "Rau",
+    data: ["Ca Rot", "Khoai Tay", "Ca Chua"]
+  },
+  {
+    title: "Proteins",
+    data: ["Egg", "Chicken", "Fish"]
+  }
+];
+
+const SectionListEx = () => {
+  return (
     <View>
-        
+      <SectionList
+        sections={Data}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section }) => <Text>{section.title}</Text>}
+      />
     </View>
-)
-}
+  );
+};
 
-export default SectionListEx
+export default SectionListEx;
