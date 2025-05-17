da617a7757b500c781bc00a87b4a67bd52d6949c
An Nguyen <anndb1903@gmail.com>
2025-05-17 16:54:58 +0700

    done

--- components/MyDrawer.tsx
+++ components/MyDrawer.tsx
@@ -1,11 +1,43 @@
-import { View } from "react-native"
+import React, { useRef } from "react";
+import {
+  DrawerLayoutAndroid,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const MyDrawer = () => {
-    return (
-        <View>
+  const drawer = useRef<DrawerLayoutAndroid>(null);
 
-        </View>
-    )
-}
+  const navigationView = () => {
+    <View>
+      <Text>Item 1</Text>
+      <Text>Item 2</Text>
+      <TouchableOpacity onPress={() => drawer.current?.closeDrawer()}>
+        <Text>Close Drawee</Text>
+      </TouchableOpacity>
+    </View>;
+  };
+
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition="left"
+      renderNavigationView={navigationView}
+    >
+      <View>
+        <TouchableOpacity onPress={() => drawer.current?.openDrawer()}>
+          <Ionicons name="menu" size={32} color="black" />
+        </TouchableOpacity>
+        <Text>Header</Text>
+      </View>
+      <View>
+        <Text>Content</Text>
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
 
 export default MyDrawer;
